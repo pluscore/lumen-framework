0e909ae4f82fc57b2d1ef214ee0a99e2fd7ccf15
selmonal <selmonal@gmail.com>
2019-03-22 10:39:26 +0800

    Add resource BelongsTo relation

---
+++ src/Resource/BelongsTo.php
@@ -0,0 +1,182 @@
+<?php
+
+namespace Plus\Resource;
+
+use Zttp\Zttp;
+use Illuminate\Support\Collection;
+use Illuminate\Database\Eloquent\Model;
+
+class BelongsTo
+{
+    /**
+     * The host name to service.
+     */
+    protected $serviceHostName;
+
+    /**
+     * The resource path to service.
+     */
+    protected $resourcePath;
+
+    /**
+     * The child model instance of the relation.
+     */
+    protected $child;
+
+    /**
+     * The foreign key of the parent model.
+     *
+     * @var string
+     */
+    protected $foreignKey;
+
+    /**
+     * The associated key on the parent model.
+     *
+     * @var string
+     */
+    protected $ownerKey;
+
+    /**
+     * The name of the relationship.
+     *
+     * @var string
+     */
+    protected $relationName;
+
+    /**
+     * Params that filtering the service.
+     *
+     * @var array
+     */
+    protected $params;
+
+    /**
+     * Create a new belongs to relationship instance.
+     *
+     * @param  string  $serviceHostName
+     * @param  string  $resourcePath
+     * @param  string  $foreignKey
+     * @param  string  $ownerKey
+     * @param  string  $relationName
+     *
+     * @return void
+     */
+    public function __construct($serviceHostName, $resourcePath, Model $child, $foreignKey, $ownerKey, $relationName)
+    {
+        $this->serviceHostName = $serviceHostName;
+        $this->resourcePath = $resourcePath;
+        $this->ownerKey = $ownerKey;
+        $this->relationName = $relationName;
+        $this->foreignKey = $foreignKey;
+
+        // In the underlying base relationship class, this variable is referred to as
+        // the "parent" since most relationships are not inversed. But, since this
+        // one is we will create a "child" variable for much better readability.
+        $this->child = $child;
+        $this->params = [];
+    }
+
+    /**
+     * Set the constraints for an eager load of the relation.
+     *
+     * @param  array  $models
+     * @return void
+     */
+    public function addEagerConstraints(array $models)
+    {
+        $this->params[$this->ownerKey] = array_unique(
+            array_pluck($models, $this->foreignKey)
+        );
+    }
+
+    /**
+     * Match the eagerly loaded results to their parents.
+     *
+     * @param  array   $models
+     * @param  \Illuminate\Database\Eloquent\Collection  $results
+     * @param  string  $relation
+     * @return array
+     */
+    public function match(array $models, Collection $results, $relation)
+    {
+        $foreign = $this->foreignKey;
+
+        $owner = $this->ownerKey;
+
+        // First we will get to build a dictionary of the child models by their primary
+        // key of the relationship, then we can easily match the children back onto
+        // the parents using that dictionary and the primary key of the children.
+        $dictionary = [];
+
+        foreach ($results as $result) {
+            $dictionary[$result[$owner]] = new Model($result);
+        }
+
+        // Once we have the dictionary constructed, we can loop through all the parents
+        // and match back onto their children using these keys of the dictionary and
+        // the primary key of the children to map them onto the correct instances.
+        foreach ($models as $model) {
+            if (isset($dictionary[$model->{$foreign}])) {
+                $model->setRelation($relation, $dictionary[$model->{$foreign}]);
+            }
+        }
+
+        return $models;
+    }
+
+    /**
+     * Initialize the relation on a set of models.
+     *
+     * @param  array   $models
+     * @param  string  $relation
+     * @return array
+     */
+    public function initRelation(array $models, $relation)
+    {
+        foreach ($models as $model) {
+            $model->setRelation($relation, null);
+        }
+
+        return $models;
+    }
+
+    /**
+     * Get the relationship for eager loading.
+     *
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function getEager()
+    {
+        return $this->get();
+    }
+
+    /**
+     * Execute the query as a "select" statement.
+     *
+     * @param  array  $columns
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
+    public function get($columns = ['*'])
+    {
+        $url = $this->getUri().'?'.http_build_query($this->params);
+
+        $response = Zttp::withHeaders(['Accept' => 'application/json'])->get($url);
+
+        if ($response->isSuccess()) {
+            return collect($response->json()['data']);
+        }
+
+        throw new \RuntimeException($response->json());
+    }
+
+    /**
+     * Get uri to the resource.
+     *
+     * @return string
+     */
+    public function getUri()
+    {
+        return "http://{$this->serviceHostName}/{$this->resourcePath}";
+    }
+}
---
+++ src/Resource/Model.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Plus\Resource;
+
+use Illuminate\Contracts\Support\Arrayable;
+
+
+class Model implements Arrayable
+{
+    /**
+     * Attributes of the model.
+     *
+     * @var array
+     */
+    protected $attributes;
+
+    /**
+     * Construct ResourceModel.
+     * 
+     * @param array $attributes
+     */
+    public function __construct($attributes)
+    {
+        $this->attributes = $attributes;
+    }
+
+    /**
+     * Convert to an array.
+     *
+     * @return array
+     */
+    public function toArray()
+    {
+        return $this->attributes;
+    }
+}
